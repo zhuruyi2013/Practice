ecc27c8b3faf69fee8a74df440bbb43dd00284f6
henry.zhu <henry.zhu@dadaabc.com>
2018-11-17 22:00:48 +0800

    bugfix : compile error
    compile error

---
+++ app/src/main/java/com/zry/shortcutdemo/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.zry.shortcutdemo
+
+import android.app.Application
+
+/**
+ * Created by zhuruyi on 2018/11/17.
+ * Copyright  2018 henzry.zhu@dadaabc.com. All rights reserved
+ */
+class MyApplication : Application() {
+
+}
